ac2696ae8a4ef9daeaee08a3d08b202a23140eda
al3xwarrior <138648975+al3xwarrior@users.noreply.github.com>
2025-10-24 14:27:33 -0400

    Dungeon 2 Work (Import RemoveItemToBeSold)
    Introduced Gatekeeper armor and weapon items (gold and leather variants) and updated CalculateItemStats.htsl to support their stat bonuses. Refactored MagmaCubes.htsl for cleaner position/velocity logic and damage calculation. Added speedrunning logic for Dungeon 2 and capped PortalEffect for high player counts. Removed Dungeon2Swing.htsl.

---
+++ DungeonHouse/Shops/AddToGetItemValue.py
@@ -0,0 +1,33 @@
+import os
+
+# Read File names in ../Items
+item_files = os.listdir('DungeonHouse\\Items')
+# Grab ItemValue script
+item_value_script = ""
+with open("DungeonHouse\\Shops\\GetItemValue.htsl", "r") as f:
+    item_value_script = f.read()
+
+for file_name in item_files:
+    if file_name.endswith(".json"):
+        item_name = file_name[:-5]
+
+        if item_name in item_value_script:
+            continue
+
+        if item_name != item_name.lower():
+            continue
+    
+        if item_name == "placeholder":
+            continue
+
+        if any(char.isdigit() for char in item_name):
+            continue
+        
+
+        print(f"Adding {item_name} to GetItemValue.htsl")
+        item_value_script += f'\nif (hasItem "../Items/{item_name}" metadata "Hand" anyAmount) {{\n    var "item_value" set 1\nexit\n}}'
+
+# Write back to GetItemValue.htsl
+with open("DungeonHouse\\Shops\\GetItemValue.htsl", "w") as f:
+    f.write(item_value_script)
+    print("Updated GetItemValue.htsl")
